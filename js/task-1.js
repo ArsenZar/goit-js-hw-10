0494b5974b94022bf1f4f8dd1314a793376a29c7
ArsenZar <arsenzar@gmail.com>
2025-04-09 20:07:26 +0300

    test Promice (true or false)

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,13 @@
+const isSuccess = true;
+
 const promise = new Promise((resolve, reject) => {
-  // Asynchronous operation
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 2000);
 });
+
+console.log(promise); // Об'єкт промісу
