c4764f23b15b408cb4acb1c54799141a6bcca4e0
ArsenZar <arsenzar@gmail.com>
2025-04-09 19:55:36 +0300

    test Promice

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,3 @@
-const startTime = Date.now();
-
-// Твій код, виконуваний упродовж деякого часу
-for(let i = 0; i <= 100; i += 1) {
-	console.log(i);
-}
-
-const endTime = Date.now();
-const elapsedTime = endTime - startTime;
-
-console.log(`Elapsed time: ${elapsedTime} ms`);
-
+const promise = new Promise((resolve, reject) => {
+  // Asynchronous operation
+});
