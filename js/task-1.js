9c7254d9ebe45c889f397a9191348ae6e32fd217
ArsenZar <arsenzar@gmail.com>
2025-04-10 23:53:43 +0300

    practice-7 (we create Promise witch share object like parametr and use paremetrs values in function)

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,18 @@
-const fetchUserFromServer = username => {
+const makePromise = ({ value, delay, shouldResolve = true }) => {
   return new Promise((resolve, reject) => {
-    console.log(`Fetching data for ${username}`);
-
     setTimeout(() => {
-      // Change value of isSuccess variable to simulate request status
-      const isSuccess = true;
-
-      if (isSuccess) {
-        resolve("success value");  // значенням параметра resolve буде колбек-функція методу then()
-      } else {
-        reject("error");  // значенням параметра reject буде колбек-функція методу catch()
-      }
-    }, 2000);
+			if(shouldResolve) {
+                resolve(console.log(value));
+			} else {
+				reject(value)
+			}
+		}, delay);
   });
 };
 
-fetchUserFromServer("Mango")
-  .then(user => console.log(user))
-  .catch(error => console.error(error));
 
+makePromise({
+	value: "Some value",
+	delay: 2000,
+	shouldResolve: true
+})
