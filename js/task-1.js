8faa2170e66c25dbde6f1bb3c016b9b671116b40
ArsenZar <arsenzar@gmail.com>
2025-04-10 23:27:46 +0300

    practice-6.1 (test promisification)

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,21 @@
-const fetchUserFromServer = (username, onSuccess, onError) => {
-  console.log(`Fetching data for ${username}`);
+const fetchUserFromServer = username => {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching data for ${username}`);
 
-  setTimeout(() => {
-    // Change value of isSuccess variable to simulate request status
-    const isSuccess = true;
+    setTimeout(() => {
+      // Change value of isSuccess variable to simulate request status
+      const isSuccess = true;
 
-    if (isSuccess) {
-      onSuccess("success value");
-    } else {
-      onError("error");
-    }
-  }, 2000);
+      if (isSuccess) {
+        resolve("success value");  // значенням параметра resolve буде колбек-функція методу then()
+      } else {
+        reject("error");  // значенням параметра reject буде колбек-функція методу catch()
+      }
+    }, 2000);
+  });
 };
 
-fetchUserFromServer(
-	"Mango", 
-	user => console.log(user), 
-	error => console.error(error)
-);
+fetchUserFromServer("Mango")
+  .then(user => console.log(user))
+  .catch(error => console.error(error));
+
