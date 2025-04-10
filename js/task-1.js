033226f2f7b0cc76d60d0289a9bde99e668bd47d
ArsenZar <arsenzar@gmail.com>
2025-04-10 17:13:31 +0300

    practice

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ const isSuccess = true;
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
+      resolve(console.log("Success! Value passed to resolve function"));
     } else {
-      reject("Error! Error passed to reject function");
+      reject(console.log("Error! Error passed to reject function"));
     }
   }, 2000);
 });
