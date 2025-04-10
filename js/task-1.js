ff834f4864f63535179e9acbae575528e9300c39
ArsenZar <arsenzar@gmail.com>
2025-04-11 01:37:06 +0300

    practice-8 (promise.rece) fix GoIT code)))

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
 const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(1), 2000);
+  setTimeout(() => resolve(1), 1000);
 });
 
 const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => rejected(2), 1000);
+  setTimeout(() => reject(2), 2000);
 });
 
 Promise.race([p1, p2])
-	.then(value => console.log(value)); 
-	.catch(error => console.log(error)); // 2
+	.then(value => console.log(value)) // 1
+	.catch(error => console.log(error));
