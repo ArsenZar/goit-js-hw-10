55517c32a0d838d3f29f78e456ac8bb450ada7fe
ArsenZar <arsenzar@gmail.com>
2025-04-11 01:25:10 +0300

    practice-8 (promise.allSettled)

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
 const p1 = Promise.resolve(1);
-const p2 = Promise.resolve(2);
+const p2 = Promise.reject("Rejected promise 2");
 const p3 = Promise.resolve(3);
 
-Promise.all([p1, p2, p3])
-	.then(values => console.log(values)) // [1, 2, 3]
-	.catch(error => console.log(error));
+Promise.allSettled([p1, p2, p3])
+	.then(values => console.log(values));
+	// [
+	//   { status: "fulfilled", value: 1}, 
+  //   { status: "rejected", value: "Rejected promise 2"},
+  //   { status: "fulfilled", value: 3}
+  // ]
