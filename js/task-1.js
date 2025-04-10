13ad25630701b013bfc5be6c740bc8d655050116
ArsenZar <arsenzar@gmail.com>
2025-04-10 17:47:58 +0300

    practice-2 .then + promise

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,10 @@
-const isSuccess = true;
+const isSuccess = false;
+const isSuccess2 = false;
 
 // Create promise
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    if (isSuccess) {
+    if (isSuccess || isSuccess2) {
       resolve("Success! Value passed to resolve function");
     } else {
       reject("Error! Error passed to reject function");
@@ -11,12 +12,33 @@ const promise = new Promise((resolve, reject) => {
   }, 2000);
 });
 
+
+/*
+ Це — імітація асинхронної операції, наприклад, запит на сервер.
+
+Якщо isSuccess === true, викликається resolve(...).
+Якщо false, викликається reject(...).
+
+Усе, що відбувається в setTimeout, в реальному житті — 
+це може бути запит на сервер, доступ до бази даних або щось, що займає час.
+*/
+
 // Registering promise callbacks
 promise.then(
-  value => {
-    console.log(value); // "Success! Value passed to resolve function"
+    value => {
+        console.log(promise);
+    //console.log(value); // "Success! Value passed to resolve function"
   },
-  error => {
-    console.log(error); // "Error! Error passed to reject function"
+    error => {
+      console.log(promise);
+   // console.log(error); // "Error! Error passed to reject function"
   }
 );
+
+/*
+А then(...) — обробник результату:
+Тобто:
+.then(onFulfilled, onRejected)
+Перший аргумент — що робити, коли все добре.
+Другий аргумент — що робити, коли сталася помилка.
+*/
