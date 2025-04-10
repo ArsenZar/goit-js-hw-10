0df9df92e7f8411cb73963cdda22baa3aa68fbda
ArsenZar <arsenzar@gmail.com>
2025-04-10 18:32:58 +0300

    practice-4 (promise, setTimeout, .then .catch)

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
-const isSuccess = false;
-const isSuccess2 = false;
+const isSuccess = true;
 
 // Create promise
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    if (isSuccess || isSuccess2) {
+    if (isSuccess) {
       resolve("Success! Value passed to resolve function");
     } else {
       reject("Error! Error passed to reject function");
@@ -12,30 +11,11 @@ const promise = new Promise((resolve, reject) => {
   }, 2000);
 });
 
-
-/*
- Це — імітація асинхронної операції, наприклад, запит на сервер.
-
-Якщо isSuccess === true, викликається resolve(...).
-Якщо false, викликається reject(...).
-
-Усе, що відбувається в setTimeout, в реальному житті — 
-це може бути запит на сервер, доступ до бази даних або щось, що займає час.
-*/
-
 // Registering promise callbacks
 promise
-    .then(value => {
-        console.log(value); // "Success! Value passed to resolve function"
-    })
-    .catch(error => {
-        console.log(error);
-  });;
-
-/*
-А then(...) — обробник результату:
-Тобто:
-.then(onFulfilled, onRejected)
-Перший аргумент — що робити, коли все добре.
-Другий аргумент — що робити, коли сталася помилка.
-*/
+  .then(value => {
+    console.log(value); // "Success! Value passed to resolve function"
+  })
+  .catch(error => {
+    console.log(error); // "Error! Error passed to reject function"
+  });
