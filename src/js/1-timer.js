861e1acc1252ebe8d2035cf0e384bc5143af5a5c
ArsenZar <arsenzar@gmail.com>
2025-08-30 12:12:09 +0300

    timer ready

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,16 +5,19 @@ import "flatpickr/dist/flatpickr.min.css";
 import "izitoast/dist/css/iziToast.min.css";
 
 const startBtn = document.querySelector('button[data-start]');
+const allSpans = document.querySelectorAll('.value');
 startBtn.disabled = true;
+let userSelectedDate = null;
 
 const options = {
     enableTime: true,
     dateFormat: "Y-m-d H:i",
     time_24hr: true,
     defaultDate: new Date(),
+    minuteIncrement: 1,
     onClose(selectedDates) {
-        const diff = selectedDates[0].getTime() - Date.now(); // різниця у мс
-        if (diff <= 0) {
+        
+        if (selectedDates[0].getTime() <= Date.now()) {
             iziToast.error({
                 message: 'Please choose a date in the future',
                 position: 'topRight',
@@ -23,14 +26,45 @@ const options = {
             });
             startBtn.disabled = true;
         } else {
-            startBtn.disabled = false; 
+            startBtn.disabled = false;
+            userSelectedDate = selectedDates[0].getTime();
         }
     },
 };
 
 flatpickr("#datetime-picker", options);
 
+startBtn.addEventListener('click', () => {
+    if (userSelectedDate == null) return;
 
+    startBtn.disabled = true;
+    document.querySelector('#datetime-picker').disabled = true;
+
+    const intervalID = setInterval(() => {
+        let diff = userSelectedDate - Date.now();
+        console.log(diff);
+        const dataTimer = convertMs(diff);
+        if (diff <= 0) {
+            clearInterval(intervalID);
+            iziToast.success({
+                title: 'OK',
+                message: 'Finish time!',
+                position: 'topRight',
+            });
+            document.querySelector('#datetime-picker').disabled = false;
+        } else {
+            updateTimerUI(dataTimer);
+        };  
+    }, 1000);
+});
+
+function updateTimerUI(dataTimer) {
+    let i = 0;
+    for (const key in dataTimer) {
+        allSpans[i].textContent = dataTimer[key];
+        i++;
+    }
+}
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -40,21 +74,20 @@ function convertMs(ms) {
     const day = hour * 24;
 
     // Remaining days
-    const days = Math.floor(ms / day);
+    const days = addLeadingZero(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
 }
 
-function addLeadingZero(timeObj) {
-    let formated = {};
-    for (const key in timeObj) {
-        formated[key] = String(timeObj[key]).padStart(2, '0');
-    }
-    return formated;
+function addLeadingZero(timeWithoutZero) {
+    return String(timeWithoutZero).padStart(2, '0');
 }
+
+
+
